8bf6518e83123a8040923d2ecdd10bbec7d1a6aa
Lane Gebbie <166470962+lanedg@users.noreply.github.com>
2025-03-03 14:14:57 -0700

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let temp = "";
+  let stringArr = string.split("");
+  for (let i = 0; i < string.length / 2; i++) {
+    temp = stringArr[stringArr.length - (i + 1)];
+    stringArr[stringArr.length - (i + 1)] = stringArr[i];
+    stringArr[i] = temp;
+  }
+  return stringArr.join("");
 };
+console.log(reverseString("Stake"));
 
 // Do not edit below this line
 module.exports = reverseString;
