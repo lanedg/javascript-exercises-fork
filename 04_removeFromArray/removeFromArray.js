c9270fe692a627bc7f593865abcc1b7b91d8b47e
Lane Gebbie <166470962+lanedg@users.noreply.github.com>
2025-03-03 14:49:24 -0700

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (array) {
+  let newArray = [];
+  for (const element of array) {
+    for (let i = 1; i < arguments.length; i++) {
+      if (element === arguments[i]) {
+        break;
+      }
+      if (i === arguments.length - 1) {
+        newArray.push(element);
+      }
+    }
+  }
+  return newArray;
 };
 
 // Do not edit below this line
